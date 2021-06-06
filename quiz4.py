ceac7b1128caa365b06d72248ebc705310094373
AnaMk16 <84284702+AnaMk16@users.noreply.github.com>
2021-06-06 19:49:41 +0400

    Add files via upload
    I've used BeautifulSoup module to fetch desirable information from web , functions like find and findall , as well as time module built-in function sleep, randint function from random module , requests module  to fetch data from imdb.com  and csv module to keep information like movie names , ranking, runtime and genre in a csv file

---
+++ quiz4.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+from time import sleep
+from random import randint
+
+
+f = open('Top_Movies.csv', 'w',encoding='utf-8_sig', newline = '\n')
+file_obj = csv.writer(f)
+file_obj.writerow(['Title','Runtime','Genre','Ranking'])
+
+
+h = {'Accept-Language':'en-US'}
+page = 1
+
+url = 'https://www.imdb.com/list/ls005337232/?st_dt=&mode=detail&page=1&sort=list_order,ascstart= ' +str(page)
+
+while page < 6:
+    r = requests.get(url, params=h)
+    #print(r)
+    c = r.text
+    soup = BeautifulSoup(c,'html.parser')
+    sec = soup.find('div', class_='lister list detail sub-list')
+    movies = sec.find_all('div', class_ = 'lister-item mode-detail')
+    
+    for each in movies:
+        title =  each.h3.a.text
+        runtime = each.find('span',class_='runtime').text
+        genre = each.find('span',class_='genre').text.strip()
+        rating = each.find('span',class_='ipl-rating-star__rating').text
+
+        file_obj.writerow([title,runtime,genre,rating])
+        print(title)
+        
+    
+    page += 1
+    sleep(randint(1,3))
+
+
